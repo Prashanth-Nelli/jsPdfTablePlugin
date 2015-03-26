a138cfca9ad19d537826d60d2c30adaf2a556a96
unknown <micprashanth@gmail.com>
2015-03-26 22:55:30 +0530

    refactor code

--- jspdf.plugin.table.js
+++ jspdf.plugin.table.js
@@ -14,9 +14,7 @@
 		var heigth;
 		var SplitIndex = [];
 		var cSplitIndex = [];
-		var indexHelper = 0;
 		var heights = [];
-		var lengths;
 		var nextStart;
 		var pageStart = 0;
 
@@ -31,285 +29,281 @@
 
 		// Inserts Table Head row
 
-	jsPDFAPI.insertHeader = function(data) {
-		var rObj = {};
-		var hObj = {};
-		rObj = data[0];
-		for (var key in rObj) {
-			hObj[key] = key;
+jsPDFAPI.insertHeader = function(data) {
+	var rObj = {};
+	var hObj = {};
+	rObj = data[0];
+	for (var key in rObj) {
+		hObj[key] = key;
+	}
+	data.unshift(hObj);
+};
+
+// intialize the dimension array, column count and row count
+
+jsPDFAPI.initPDF = function(data, marginConfig, firstpage) {
+
+	dimensions[0] = marginConfig.xstart;
+
+	if (firstpage) {
+		dimensions[1] = marginConfig.tablestart;
+	} else {
+		dimensions[1] = marginConfig.ystart;
+	}
+
+	dimensions[2] = this.internal.pageSize.width - marginConfig.xstart - 20 - marginConfig.marginright;
+	dimensions[3] = 250;
+	dimensions[4] = marginConfig.ystart;
+	dimensions[5] = marginConfig.marginright;
+	dimensions[6] = marginConfig.xOffset || 5;
+	dimensions[7] = marginConfig.yOffset || 5;
+
+	columnCount = this.calColumnCount(data);
+	rowCount = data.length;
+	width = dimensions[2] / columnCount;
+	height = dimensions[2] / rowCount;
+	dimensions[3] = this.calculateDim(data, dimensions);
+
+};
+
+//draws table on the document
+
+jsPDFAPI.drawTable = function(table_DATA, marginConfig) {
+
+	var self = this;
+	var i = 0;
+	var j = 0;
+	var tabledata = [];
+
+	if (!marginConfig) {
+		marginConfig = {};
+	}
+
+	for (var key in defaultConfig) {
+		if (marginConfig[key]) {
+			defaultConfig[key] = marginConfig[key];
 		}
-		data.unshift(hObj);
-	};
+	}
 
-	// intialize the dimension array, column count and row count
+	pageStart = defaultConfig.tablestart;
 
-	jsPDFAPI.initPDF = function(data, marginConfig, firstpage) {
+	this.initPDF(table_DATA, defaultConfig, true);
 
-		dimensions[0] = marginConfig.xstart;
-
-		if (firstpage) {
-			dimensions[1] = marginConfig.tablestart;
-		} else {
-			dimensions[1] = marginConfig.ystart;
-		}
-
-		dimensions[2] = this.internal.pageSize.width - marginConfig.xstart - 20 - marginConfig.marginright;
-		dimensions[3] = 250;
-		dimensions[4] = marginConfig.ystart;
-		dimensions[5] = marginConfig.marginright;
-		dimensions[6] = marginConfig.xOffset || 5;
-		dimensions[7] = marginConfig.yOffset || 5;
-
-		columnCount = this.calColumnCount(data);
-		rowCount = data.length;
-		width = dimensions[2] / columnCount;
-		height = dimensions[2] / rowCount;
-		dimensions[3] = this.calculateDim(data, dimensions);
-
-	};
-
-	//draws table on the document
-
-	jsPDFAPI.drawTable = function(table_DATA, marginConfig) {
-
-		var self = this;
-		var i = 0;
-		var j = 0;
-		var tabledata = [];
-
-		SplitIndex = [];
-		cSplitIndex = [];
-		indexHelper = 0;
-		heights = [];
-
-		if (!marginConfig) {
-			marginConfig = {};
-		}
-
-		for (var key in defaultConfig) {
-			if (marginConfig[key]) {
-				defaultConfig[key] = marginConfig[key];
+	if ((dimensions[3] + defaultConfig.tablestart) > (self.internal.pageSize.height)) {
+		cSplitIndex = SplitIndex;
+		cSplitIndex.push(table_DATA.length);
+		for (; i < cSplitIndex.length; i++) {
+			tabledata = [];
+			tabledata = table_DATA.slice(j, cSplitIndex[i]);
+			this.insertHeader(tabledata);
+			this.pdf(tabledata, dimensions, true, false);
+			pageStart = defaultConfig.ystart;
+			this.initPDF(tabledata, defaultConfig, false);
+			jg = cSplitIndex[i];
+			if ((i + 1) != cSplitIndex.length) {
+				this.addPage();
 			}
 		}
+	} else {
+		this.insertHeader(table_DATA)
+		this.pdf(table_DATA, dimensions, true, false);
+	}
 
-		pageStart = defaultConfig.tablestart;
+	return nextStart;
+};
 
-		this.initPDF(table_DATA, defaultConfig, true);
+//calls methods in a sequence manner required to draw table
 
-		if ((dimensions[3] + defaultConfig.tablestart) > (self.internal.pageSize.height)) {
-			cSplitIndex = SplitIndex;
-			cSplitIndex.push(table_DATA.length);
-			for (; i < cSplitIndex.length; i++) {
-				tabledata = [];
-				tabledata = table_DATA.slice(j, cSplitIndex[i]);
-				this.insertHeader(tabledata);
-				this.pdf(tabledata, dimensions, true, false);
-				pageStart = defaultConfig.ystart;
-				this.initPDF(tabledata, defaultConfig, false);
-				jg = cSplitIndex[i];
-				if ((i + 1) != cSplitIndex.length) {
-					this.addPage();
-				}
-			}
-		} else {
-			this.insertHeader(table_DATA)
-			this.pdf(table_DATA, dimensions, true, false);
-		}
+jsPDFAPI.pdf = function(table, dimensions, hControl, bControl) {
+	columnCount = this.calColumnCount(table);
+	rowCount = table.length;
+	dimensions[3] = this.calculateDim(table, dimensions);
+	width = dimensions[2] / columnCount;
+	height = dimensions[2] / rowCount;
+	this.drawRows(rowCount, dimensions, hControl);
+	this.drawColumns(columnCount, dimensions);
+	nextStart = this.insertData(rowCount, columnCount, dimensions, table, bControl);
+	return nextStart;
+};
 
-		return nextStart;
-	};
+//inserts text into the table
 
-	//calls methods in a sequence manner required to draw table
+jsPDFAPI.insertData = function(rowCount, columnCount, dimensions, data, brControl) {
+	var self = this;
+	var fontSize = self.internal.getFontSize();
+	var xOffset = defaultConfig.xOffset;
+	var yOffset = defaultConfig.yOffset;
+	var iTexts = 0;
+	var cell = null;
+	var start = 0;
+	var end = 0;
+	var obj = {};
 
-	jsPDFAPI.pdf = function(table, dimensions, hControl, bControl) {
-		columnCount = this.calColumnCount(table);
-		rowCount = table.length;
-		dimensions[3] = this.calculateDim(table, dimensions);
-		width = dimensions[2] / columnCount;
-		height = dimensions[2] / rowCount;
-		this.drawRows(rowCount, dimensions, hControl);
-		this.drawColumns(columnCount, dimensions);
-		nextStart = this.insertData(rowCount, columnCount, dimensions, table, bControl);
-		return nextStart;
-	};
+	y = dimensions[1] + yOffset;
 
-	//inserts text into the table
-
-	jsPDFAPI.insertData = function(rowCount, columnCount, dimensions, data, brControl) {
-		var self = this;
-		var fontSize = self.internal.getFontSize();
-		var xOffset = defaultConfig.xOffset;
-		var yOffset = defaultConfig.yOffset;
-		var iTexts = 0;
-		var cell = null;
-		var start = 0;
-		var end = 0;
-		var obj = {};
-
-		y = dimensions[1] + yOffset;
-
-		for (var i = 0; i < rowCount; i++) {
-			obj = data[i];
-			x = dimensions[0] + xOffset;
-			for (var key in obj) {
-				if (obj.hasOwnProperty(key)) {
-
-					cell = (obj[key] ? obj[key] : '-') + '';
-
-					if (((cell.length * fontSize) + xOffset) > (width)) {
-						iTexts = cell.length * fontSize;
-						start = 0;
-						end = 0;
-						ih = 0;
-						if ((brControl) && (i === 0)) {
-							self.setFont(self.getFont().fontName, "bold");
-						}
-						for (var j = 0; j < iTexts; j++) {
-							end += Math.floor(2 * width / fontSize) - Math.ceil(xOffset / fontSize);
-							this.text(x, y + ih, cell.substring(start, end));
-							start = end;
-							ih += fontSize;
-						}
-					} else {
-						if ((brControl) && (i === 0)) {
-							this.setFont("times", "bold");
-						}
-						this.text(x, y, cell);
-					}
-					x += dimensions[2] / columnCount;
-				}
-			}
-			this.setFont("times", "normal");
-			y += heights[i];
-		}
-		return y;
-	};
-
-	//calculates no.of based on the data array
-
-	jsPDFAPI.calColumnCount = function(data) {
-		var obj = data[0];
-		var i = 0;
+	for (var i = 0; i < rowCount; i++) {
+		obj = data[i];
+		x = dimensions[0] + xOffset;
 		for (var key in obj) {
 			if (obj.hasOwnProperty(key)) {
-				i += 1;
-			}
-		}
-		return i;
-	};
 
-	//draws columns based on the caluclated dimensionsensions
+				cell = (obj[key] ? obj[key] : '-') + '';
 
-	jsPDFAPI.drawColumns = function(i, dimensions) {
-		var x = dimensions[0];
-		var y = dimensions[1];
-		var w = dimensions[2] / i;
-		var h = dimensions[3];
-
-		for (var j = 0; j < i; j++) {
-			this.rect(x, y, w, h);
-			x += w;
-		}
-	};
-
-	//calculates dimensionsensions based on the data array and returns y position for further editing of document
-
-	jsPDFAPI.calculateDim = function(data, dimensions) {
-		var row = 0;
-		var x = dimensions[0];
-		var y = dimensions[1];
-		var self = this;
-		var fontSize = self.internal.getFontSize();
-		var noOfLines = 0;
-		lengths = [];
-		heights = [];
-		value = 0;
-		indexHelper = 0;
-		SplitIndex = [];
-
-		for (var i = 0; i < data.length; i++) {
-			var obj = data[i];
-			var length = 0;
-			for (var key in obj) {
-				if (obj[key] !== null) {
-					if (length < obj[key].length) {
-						lengths[row] = obj[key].length;
-						length = lengths[row];
+				if (((cell.length * fontSize) + xOffset) > (width)) {
+					iTexts = cell.length * fontSize;
+					start = 0;
+					end = 0;
+					ih = 0;
+					if ((brControl) && (i === 0)) {
+						self.setFont(self.getFont().fontName, "bold");
 					}
+					for (var j = 0; j < iTexts; j++) {
+						end += Math.floor(2 * width / fontSize) - Math.ceil(xOffset / fontSize);
+						this.text(x, y + ih, cell.substring(start, end));
+						start = end;
+						ih += fontSize;
+					}
+				} else {
+					if ((brControl) && (i === 0)) {
+						this.setFont("times", "bold");
+					}
+					this.text(x, y, cell);
 				}
-			}++row;
-		}
-
-		for (var i = 0; i < lengths.length; i++) {
-			if ((lengths[i] * (fontSize)) > (width - dimensions[5])) {
-				noOfLines = Math.ceil((lengths[i] * (fontSize)) / width);
-				heights[i] = (noOfLines) * (fontSize / 2) + dimensions[6] + 10;
-			} else {
-				heights[i] = (fontSize + (fontSize / 2)) + dimensions[6] + 10;
+				x += dimensions[2] / columnCount;
 			}
 		}
+		this.setFont("times", "normal");
+		y += heights[i];
+	}
+	return y;
+};
 
-		for (var i = 0; i < heights.length; i++) {
-			value += heights[i];
-			indexHelper += heights[i];
-			if (indexHelper > (self.internal.pageSize.height - pageStart)) {
-				SplitIndex.push(i);
-				indexHelper = 0;
-				pageStart = dimensions[4] + 30;
-			}
+//calculates no.of based on the data array
+
+jsPDFAPI.calColumnCount = function(data) {
+	var obj = data[0];
+	var i = 0;
+	for (var key in obj) {
+		if (obj.hasOwnProperty(key)) {
+			i += 1;
 		}
+	}
+	return i;
+};
 
-		return value;
-	};
+//draws columns based on the caluclated dimensionsensions
 
-	//draw rows based on the length of data array
+jsPDFAPI.drawColumns = function(i, dimensions) {
+	var x = dimensions[0];
+	var y = dimensions[1];
+	var w = dimensions[2] / i;
+	var h = dimensions[3];
 
-	jsPDFAPI.drawRows = function(i, dimensions, hrControl) {
+	for (var j = 0; j < i; j++) {
+		this.rect(x, y, w, h);
+		x += w;
+	}
+};
 
-		var x = dimensions[0];
-		var y = dimensions[1];
-		var w = dimensions[2];
-		var h = dimensions[3] / i;
+//calculates dimensionsensions based on the data array and returns y position for further editing of document
 
-		for (var j = 0; j < i; j++) {
-			if (j === 0 && hrControl) {
-				this.setFillColor(182, 192, 192);
-				//colour combination for table header
-				this.rect(x, y, w, heights[j], 'F');
-			} else {
-				this.setDrawColor(0, 0, 0);
-				//colour combination for table borders you
-				this.rect(x, y, w, heights[j]);
+jsPDFAPI.calculateDim = function(data, dimensions) {
+	var row = 0;
+	var x = dimensions[0];
+	var y = dimensions[1];
+	var self = this;
+	var fontSize = self.internal.getFontSize();
+	var noOfLines = 0;
+	var indexHelper = 0;
+	var lengths = [];
+	
+	heights = [];
+	value = 0;
+	SplitIndex = [];
+
+	for (var i = 0; i < data.length; i++) {
+		var obj = data[i];
+		var length = 0;
+		for (var key in obj) {
+			if (obj[key] !== null) {
+				if (length < obj[key].length) {
+					lengths[row] = obj[key].length;
+					length = lengths[row];
+				}
 			}
-			y += heights[j];
+		}++row;
+	}
+
+	for (var i = 0; i < lengths.length; i++) {
+		if ((lengths[i] * (fontSize)) > (width - dimensions[5])) {
+			noOfLines = Math.ceil((lengths[i] * (fontSize)) / width);
+			heights[i] = (noOfLines) * (fontSize / 2) + dimensions[6] + 10;
+		} else {
+			heights[i] = (fontSize + (fontSize / 2)) + dimensions[6] + 10;
 		}
-	};
+	}
 
-	//converts table to json
+	for (var i = 0; i < heights.length; i++) {
+		value += heights[i];
+		indexHelper += heights[i];
+		if (indexHelper > (self.internal.pageSize.height - pageStart)) {
+			SplitIndex.push(i);
+			indexHelper = 0;
+			pageStart = dimensions[4] + 30;
+		}
+	}
 
-	jsPDFAPI.tableToJson = function(id) {
+	return value;
+};
 
-		var table = document.getElementById(id);
-		var keys = [];
-		var rows = table.rows;
-		var noOfRows = rows.length;
-		var noOfCells = table.rows[0].cells.length;
-		var i = 0;
-		var j = 0;
-		var data = [];
-		var obj = {};
+//draw rows based on the length of data array
 
+jsPDFAPI.drawRows = function(i, dimensions, hrControl) {
+
+	var x = dimensions[0];
+	var y = dimensions[1];
+	var w = dimensions[2];
+	var h = dimensions[3] / i;
+
+	for (var j = 0; j < i; j++) {
+		if (j === 0 && hrControl) {
+			this.setFillColor(182, 192, 192);
+			//colour combination for table header
+			this.rect(x, y, w, heights[j], 'F');
+		} else {
+			this.setDrawColor(0, 0, 0);
+			//colour combination for table borders you
+			this.rect(x, y, w, heights[j]);
+		}
+		y += heights[j];
+	}
+};
+
+//converts table to json
+
+jsPDFAPI.tableToJson = function(id) {
+
+	var table = document.getElementById(id);
+	var keys = [];
+	var rows = table.rows;
+	var noOfRows = rows.length;
+	var noOfCells = table.rows[0].cells.length;
+	var i = 0;
+	var j = 0;
+	var data = [];
+	var obj = {};
+
+	for ( i = 0; i < noOfCells; i++) {
+		keys.push(rows[0].cells[i].textContent);
+	}
+
+	for ( j = 0; j < noOfRows; j++) {
+		obj = {};
 		for ( i = 0; i < noOfCells; i++) {
-			keys.push(rows[0].cells[i].textContent);
-		}
-
-		for ( j = 0; j < noOfRows; j++) {
-			obj = {};
-			for ( i = 0; i < noOfCells; i++) {
-				try {
-					obj[keys[i]] = rows[j].cells[i].textContent.replace(/^\s+|\s+$/gm, '');
-				} catch(ex) {
-					obj[keys[i]] = '';
+			try {
+				obj[keys[i]] = rows[j].cells[i].textContent.replace(/^\s+|\s+$/gm, '');
+			} catch(ex) {
+				obj[keys[i]] = '';
 				}
 			}
 			data.push(obj);
